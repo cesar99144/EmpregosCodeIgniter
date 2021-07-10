fe567e5c1408d81db052889835185712a52f5de9
César Ferreira <cesarferreiraa67@gmail.com>
2021-07-10 20:47:23 -0300

    Conexao banco

--- empregos/app/Config/Database.php
+++ empregos/app/Config/Database.php
@@ -36,7 +36,7 @@ class Database extends Config
 		'username' => 'root',
 		'password' => '12345',
 		'database' => 'empregosconecta',
-		'DBDriver' => 'MySQLi',
+		'DBDriver' => 'mysqli',
 		'DBPrefix' => '',
 		'pConnect' => false,
 		'DBDebug'  => (ENVIRONMENT !== 'production'),
