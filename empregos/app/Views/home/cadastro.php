e6b269399f7cad2967f7298134269a310a2bd034
César Ferreira <cesarferreiraa67@gmail.com>
2021-07-10 20:48:11 -0300

    Tela cadastro recrutador

---
+++ empregos/app/Views/home/cadastro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="nome" placeholder="Nome"><br>
+        <input type="text" name="sobrenome" placeholder="Sobrenome"><br>
+        <input type="text" name="cnpj" placeholder="CNPJ da empresa em que você representa"><br>
+        <input type="email" name="email" placeholder="Email"><br>
+        <input type="password" name="senha" placeholder="Senha"><br>
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
