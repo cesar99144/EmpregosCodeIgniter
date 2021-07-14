c6d1f0908f74af3872a61405c217ed0c47211871
César Ferreira <cesarferreiraa67@gmail.com>
2021-07-13 23:03:43 -0300

    Correção da classe usuário para o singular

--- empregos/app/Controllers/Usuario.php
+++ empregos/app/Controllers/Usuario.php
@@ -2,10 +2,28 @@
 
 namespace App\Controllers;
 
-class Usuarios extends BaseController{
+class Usuario extends BaseController{
 
     public function cadastrarUsuario(){
 
-        
+        if($this->request->getMethod() === 'post'){
+
+            $usuariosModel = new \App\Models\UsuariosModel();
+            $usuariosModel->set('nome', $this->request->getPost('nome'));
+            $usuariosModel->set('sobrenome', $this->request->getPost('sobrenome'));
+            $usuariosModel->set('cnpj', $this->request->getPost('cnpj'));
+            $usuariosModel->set('nivelRepresentante', $this->request->getPost('nivelRepresentante'));
+            $usuariosModel->set('email', $this->request->getPost('email'));
+            $usuariosModel->set('senha', $this->request->getPost('senha'));
+
+            if($usuariosModel->insert()){
+
+                //Caso consiga salvar os dados
+
+            }else{
+
+                
+            }
+        }
     }
 }
