bfe65420171d03f36e0dfa8720dada4a7c0034e9
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-03 22:17:00 -0300

    Model dos candidatos

---
+++ empregos/app/Models/CandidatosModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class CandidatosModel extends Model{
+
+    protected $table = 'candidatos';
+    protected $primaryKey = 'idCandidato';
+    protected $allowedFields = ['nome','sobrenome', 'celular', 'email', 'senha', 'estado', 'cidade', 'formacao', 'ultimaexperiencia', 'areaPretendida', 'faixaSalarialPretendida'];
+}
