6d5dd66b84d922970322c258b6511f2838d673b3
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-03 21:47:07 -0300

    Create 2021-12-04-000157_Usuarios.php

---
+++ empregos/app/Database/Migrations/2021-12-04-000157_Usuarios.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Usuarios extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unique' => true,
+                'auto_increment' => true,
+            ],
+
+			'nome' => [
+				'type' => 'VARCHAR',
+				'constraint' => 60
+			],
+
+			'sobrenome' => [
+				'type' => 'VARCHAR',
+				'constraint' => 60
+			],
+
+			'nivelRepresentante' => [
+				'type' => 'VARCHAR',
+				'constraint' => 40
+			],
+
+			'email' => [
+				'type' => 'VARCHAR',
+				'constraint' => 254
+			],
+
+			'senha' => [
+				'type' => 'VARCHAR',
+				'constraint' => 40
+			],
+
+            'empresaId' => [
+                'type' => 'INT',
+            ],
+		]);
+
+		$this->forge->addPrimaryKey('id', TRUE);
+        $this->forge->addForeignKey('empresaId', 'empresas', 'idEmpresa');
+		$this->forge->createTable('usuarios', TRUE);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
