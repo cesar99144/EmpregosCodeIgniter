07e68ba16bc010a65e396eb604fd2dbacf9ab1e2
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-14 21:42:18 -0300

    Arquivo autenticações

---
+++ empregos/app/Auth.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace App;
+use App\Models\CandidatosModel;
+
+class Auth{
+
+    public function loginRecrutador(){
+
+
+    }
+
+    public function loginCandidatoApp($email, $senha){
+
+        $candidatosModel = new CandidatosModel;
+
+        $data = $candidatosModel->where('email', $email)->first();
+
+        if($data):
+
+            $senhaUser = $data['senha'];
+
+            $autenticacaoSenha = $senhaUser === $senha;
+           
+            if($autenticacaoSenha):
+                
+                return true;
+
+            else:
+
+                return false;
+                
+            endif;
+
+        else:
+
+            return true;
+
+        endif;
+    }
+}
