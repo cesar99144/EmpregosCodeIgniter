f98980b8a1a2fa3fd845302402ebe71dbef60c1b
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-27 21:37:55 -0300

    Create 2021-12-28-000133_CreateVagasCandidatos.php

---
+++ empregos/app/Database/Migrations/2021-12-28-000133_CreateVagasCandidatos.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateVagasCandidatos extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'idCandidatura' => [
+                'type' => 'INT',
+                'unique' => true,
+                'auto_increment' => true,
+            ],
+
+            'vagaId' => [
+                'type' => 'INT',
+            ],
+
+            'candidatoId' => [
+                'type' => 'INT',
+            ],
+
+            'dataCandidatura' => [
+                'type' => 'DATE',
+                'default' => NULL
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('idCandidatura', TRUE);
+        $this->forge->addForeignKey('vagaId', 'vagas', 'idVaga');
+        $this->forge->addForeignKey('candidatoId', 'candidatos', 'idCandidato');
+        $this->forge->createTable('vagasCandidaturas', TRUE);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
