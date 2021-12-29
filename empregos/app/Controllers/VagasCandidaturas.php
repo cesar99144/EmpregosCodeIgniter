356fc74e5bc6a0da4240970cb72e7aff0372cc8f
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-28 21:07:45 -0300

    Método de armazenar dados de candidaturas

--- empregos/app/Controllers/VagasCandidaturas.php
+++ empregos/app/Controllers/VagasCandidaturas.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
+use App\Models\VagasCandidaturasModel;
 
 class VagasCandidaturas extends ResourceController{
 
@@ -11,5 +12,29 @@ class VagasCandidaturas extends ResourceController{
     public function index(){
 
         echo "Teste";
+    }
+
+    public function create(){
+
+        $vagasCandidaturasModel = new VagasCandidaturasModel();
+
+        $data = $this->request->getJSON();
+
+        if($vagasCandidaturasModel->insert($data)){
+            
+            $response = [
+                'status'   => 201,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Dados salvos'
+                ]
+            ];
+            return $this->respondCreated($response);
+
+        }else{
+
+            return $this->fail($model->errors());
+            
+        }
     }
 }
