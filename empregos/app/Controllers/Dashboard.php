e8e48e2398f180c590a48fb0fc3955245d67d50f
César Ferreira <cesarferreiraa67@gmail.com>
2022-01-15 22:00:24 -0300

    Metódos de carregamento das telas do dashboard

--- empregos/app/Controllers/Dashboard.php
+++ empregos/app/Controllers/Dashboard.php
@@ -1,11 +1,37 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\VagasModel;
+use App\Models\CategoriasModel;
 
 class Dashboard extends BaseController{
 
     public function home(){
 
-        return view('moduloRecrutador/vagas/listagem');
+        $vagasModel = new VagasModel;
+        $data['listVagas'] = $vagasModel->where('empresaId', session()->idUser)->join('categorias', 'vagas.categoriaId = categorias.idCategoria')->findAll();
+        
+        return view('moduloRecrutador/vagas/listagem', $data);
+    }
+
+    public function telaVagasAbertas(){
+
+        return view('moduloRecrutador/vagas/processosAbertos');
+    }
+
+    public function telaCadastrarVagas(){
+
+        $categoriasModel = new CategoriasModel;
+        $data['listagemCategorias'] = $categoriasModel->where('status', 'Ativo')->findAll();
+
+        return view('moduloRecrutador/vagas/cadastrarVaga', $data);
+    }
+
+    public function telaPesquisarCandidatos(){
+
+        $categoriasModel = new CategoriasModel;
+        $data['listagemCategorias'] = $categoriasModel->where('status', 'Ativo')->findAll();
+
+        return view('moduloRecrutador/candidatos/pesquisarPerfil', $data);
     }
 }
