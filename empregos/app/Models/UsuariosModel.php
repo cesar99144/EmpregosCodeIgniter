0020d4a9e65d45756fc78fe1571a439d3593a7b4
César Ferreira <cesarferreiraa67@gmail.com>
2022-01-15 22:01:30 -0300

    Campos dos usuários

--- empregos/app/Models/UsuariosModel.php
+++ empregos/app/Models/UsuariosModel.php
@@ -7,7 +7,16 @@ class UsuariosModel extends Model{
 
     protected $table = 'usuarios';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['nome', 'sobrenome', 'cnpj', 'nivelRepresentante', 'email', 'senha'];
+    protected $allowedFields = [
+        'nome', 
+        'sobrenome', 
+        'nivelRepresentante', 
+        'email',
+        'senha',
+        'empresaId',
+        'statusUsuario'
+    ];
+    protected $returnType = 'object';
 
     public function loginUsuario($email, $senha){
 
