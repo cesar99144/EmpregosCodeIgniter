7617735a874658361a9ec60ea2157bbbea5259cf
César Ferreira <cesarferreiraa67@gmail.com>
2022-01-15 22:02:34 -0300

    Tela listar vagas no dashboard

--- empregos/app/Views/moduloRecrutador/vagas/listagem.php
+++ empregos/app/Views/moduloRecrutador/vagas/listagem.php
@@ -3,10 +3,13 @@
 
     <link rel="stylesheet" href="<?= base_url('assets/css/dashboard/vagasListagem.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/dashboard/sessao.css') ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
+
 
     <div class="container d-flex justify-content-between topoConteudo">
         <div>
-            <h2>Vagas</h2>
+            <h2 class="tituloTexto">Vagas</h2>
         </div>
         <div>
             <a href="#" class="btn btn-primary btn-sm">Adicionar vaga</a>
@@ -14,7 +17,7 @@
     </div>
 
     <div class="container">
-        <table class="content-table text-center">
+        <table id="tabelaVagas" class="content-table text-center">
             <thead>
                 <tr>
                     <th>Código</th>
@@ -26,128 +29,40 @@
                 </tr>
             </thead>
             <tbody>
+                <?php foreach($listVagas as $vaga): ?>
                 <tr>
-                    <td>35</td>
-                    <td>Representante comercial</td>
-                    <td>CLT</td>
-                    <td>Presencial</td>
-                    <td>Vendas</td>
+                    <td><?php echo $vaga->idVaga; ?></td>
+                    <td><?php echo $vaga->titulo; ?></td>
+                    <td><?php echo $vaga->RegimeContratacao; ?></td>
+                    <td><?php echo $vaga->cargaHoraria; ?></td>
+                    <td><?php echo $vaga->tituloCategoria; ?></td>
                     <td>
                         <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
                         <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
                     </td>
                 </tr>
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td>33</td>
-                    <td>Programador php</td>
-                    <td>CLT</td>
-                    <td>Remoto</td>
-                    <td>Tecnologia</td>
-                    <td>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                        <a href="" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                    </td>
-                </tr>
-                
+                <?php endforeach; ?>
             </tbody>
     </table>
 
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
+    
+    <script>
+        $(document).ready(function() {
+            $('#tabelaVagas').DataTable({
+                "searching": true,
+                "language": {
+                    "lengthMenu": "Exibindo _MENU_ registros por página",
+                    "zeroRecords": "Nenhum dado cadastrado",
+                    "info": "Exibindo página _PAGE_ de _PAGES_",
+                    "infoEmpty": "Nenhum dado cadastrado",
+                    "infoFiltered": "(filtered from _MAX_ total records)"
+                }
+            });
+        });
+    </script>
 <?= $this->endSection(); ?>
