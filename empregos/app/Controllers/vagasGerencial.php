35ac15f01e25c5f955865c4017cce425053d6b57
César Ferreira <cesarferreiraa67@gmail.com>
2022-01-15 22:00:31 -0300

    Create vagasGerencial.php

---
+++ empregos/app/Controllers/vagasGerencial.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Controllers;
+use CodeIgniter\RESTful\ResourceController;
+
+class VagasGerencial extends ResourceController{
+    
+    public function index(){
+
+    }
+
+    public function show($id = null){
+
+
+    }
+
+    public function new(){
+
+        
+    }
+}
